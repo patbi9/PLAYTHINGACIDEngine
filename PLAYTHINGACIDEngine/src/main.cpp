67283f99d952a51cd07c82d4a03b2e6cb1527d67
patbi9 <cortiz38210@ucq.edu.mx>
2025-05-14 11:23:50 -0600

    Setup Render Pipeline

--- PLAYTHINGACIDEngine/src/main.cpp
+++ PLAYTHINGACIDEngine/src/main.cpp
@@ -1,37 +1,63 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    // Crear la ventana
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Ejemplo simple de SFML");
+sf::RenderWindow* window;
+sf::CircleShape* circle;
+sf::RectangleShape* square;
 
-    // Crear un cuadrade
-    sf::RectangleShape square(sf::Vector2f(190.f, 190.f)); // ancho y alto
-    square.setFillColor(sf::Color::White);
-    square.setPosition(200.f, 150.f);
+void
+init() {
+    window = new sf::RenderWindow(sf::VideoMode(800, 600), "PLAYTHINGACIDEngine");
 
-    // Crear un círculo
-    sf::CircleShape circle(100.f); // radio 100
-    circle.setFillColor(sf::Color::White);
-    circle.setPosition(400.f, 150.f);
+    circle = new sf::CircleShape(100.0f);
+    circle->setFillColor(sf::Color::White);
+    circle->setPosition(400.f, 150.f);
 
-    // Bucle principal
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Cerrar la ventana si el usuario lo indica
-            if (event.type == sf::Event::Closed)
-                window.close();
+    square = new sf::RectangleShape(sf::Vector2f(190.f, 190.f));
+    square->setFillColor(sf::Color::White);
+    square->setPosition(200.f, 150.f);
+}
+
+void
+handleEvents() {
+    sf::Event event;
+    while (window->pollEvent(event)) {
+        // Cerrar la ventana si el usuario lo indica
+        if (event.type == sf::Event::Closed) {
+            window->close();
         }
+    }
+}
 
-        // Limpiar, dibujar y mostrar
-        window.clear();
-        window.draw(square);
-        window.draw(circle);
-        window.display();
+void
+update() {
+
+}
+
+void
+render() {
+    // Limpiar, dibujar y mostrar
+    window->clear();
+    window->draw(*square);
+    window->draw(*circle);
+    window->display();
+}
+
+void
+destroy() {
+
+}
+
+//esto es el gameloop
+int
+main() {
+    init();
+
+    while (window->isOpen()) {
+        handleEvents();
+        update();
+        render();
     }
 
+    destroy();
     return 0;
 }
